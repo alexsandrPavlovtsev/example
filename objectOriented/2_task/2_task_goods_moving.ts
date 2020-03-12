87ec5b70c7f65dd513858cba088484a40cc7fe14
alexsandrPavlovtsev <alexandr.pavlovtsev@gmail.com>
2020-03-12 18:08:18 +0300

    start realization 2_task

--- objectOriented/2_task/2_task_goods_moving.ts
+++ objectOriented/2_task/2_task_goods_moving.ts
@@ -1,27 +1,93 @@
+const randomNumberGenerate = function(min: number, max: number): number {
+        const rand = min + Math.random() * (max + 1 - min);
+        return rand;
+}
 class ProductProducer {
-    producerName: string
+    _producerName: string;
+    _productCount: number;
+
     constructor(producerName: string) {
-        this.producerName = producerName
+        this._producerName = producerName
+        this._productCount = 0
+    }
+    set producerName (producerName: string) {
+        this._producerName = producerName;
+    }
+    get producerName(): string {
+        return this._producerName;
+    }
+    set productCounts (productCount: number) {
+        this._productCount = productCount;
+    }
+    get productCounts(): number {
+        return this._productCount;
+    }
+    randomGenerateProducts (): void {
+        this._productCount = randomNumberGenerate(50, 150);
     }
 }
 class Product extends ProductProducer {
-    productName: string;
+    _productName: string;
     constructor(producerName: string,productName: string) {
         super(producerName);
-        this.productName = productName;
+        this._productName = productName;
+    }
+    set productName(productName: string) {
+        this._productName = productName;
+    }
+    get productName(): string {
+        return this._productName;
     }
 }
 class ProductConsumer extends Product {
-    consumerName: any;
+    _consumerName: string;
+    _consumerNeeds: number;
     constructor(producerName: string,productName: string, consumerName: string) {
         super(producerName,productName);
-        this.consumerName = consumerName;
+        this._consumerName = consumerName;
+        this._consumerNeeds = 0;
+    }
+    set consumerName(consumerName: string) {
+        this._consumerName = consumerName;
+    }
+    get consumerName(): string {
+        return this._consumerName;
+    }
+    set consumerNeeds(consumerNeeds: number) {
+        this._consumerNeeds = consumerNeeds;
+    }
+    get consumerNeeds(): number {
+        return this._consumerNeeds;
+    }
+    consumerNeed(): void {
+        this._consumerNeeds = randomNumberGenerate(70, 120);
     }
 }
 class Agent extends ProductConsumer {
-    agentName: string;
+    _agentName: string;
+    _totalShippingProducts: number;
     constructor(producerName: string,productName: string, consumerName: string, agentName: string) {
         super(producerName,productName, consumerName);
-        this.agentName = agentName;
+        this._agentName = agentName;
+        this._totalShippingProducts = 0;
     }
-} 
+    set agentName(agentName: string) {
+        this._agentName = agentName;
+    }
+    get agentName(): string {
+        return this._agentName;
+    }
+    calculateShippingGoods(): void {
+        this._totalShippingProducts = randomNumberGenerate(0, 100);
+    }
+}
+class Day {
+    _productProducerGoodsCount: number;
+    constructor() {
+        this._productProducerGoodsCount = 0;
+    }
+    startDay() {
+        const productProducter = new ProductProducer('Alibaba');
+        const product = new Product('Alibaba', 'Super dicer');
+    }
+}
